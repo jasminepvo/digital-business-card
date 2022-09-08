f78a904180d2decf14ada501bdaa7f57f50d3efb
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-08 08:38:01 -0400

    Fix footer size + make website a link

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,13 @@ export default function Main() {
 		<div className="main">
 			<h1>Krista Chanarittichai</h1>
 			<h3>Data Engineer</h3>
-			<p>kristach.netlify.app</p>
+			<a href="https://krista.netlify.app">krista.netlify.app</a>
 			<button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="15"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm17 4.238l-7.928 7.1L4 7.216V19h16V7.238zM4.511 5l7.55 6.662L19.502 5H4.511z" fill="rgba(30,31,38,1)"/></svg>Email</button>
 			<section>
 				<h2>About</h2>
 				<p>I am a data engineer with a love for technology and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
 				<h2>Interests</h2>
-				<p>Thai. Food lover. Avid reader. Travel fanatic. Cat mom. </p>
+				<p>Native Thai. Food lover. Avid reader. Travel fanatic. Cat mom. Practicing self development. Photogenic. Sunset chaser.</p>
 			</section>
 		</div>
 	);
